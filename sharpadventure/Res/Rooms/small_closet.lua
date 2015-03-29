4ca3d8ca7d1c2d74022765b27c078494a1323bc0
Alek Ratzloff <alekratz@gmail.com>
2015-03-29 16:00:59 -0400

    Add another test room that you can go to! oh boy

---
+++ sharpadventure/Res/Rooms/small_closet.lua
@@ -0,0 +1,11 @@
+name = "a small closet"
+shortname = "small_closet"
+description = "A small, cramped closet for storing various wares, most of which are not of interest to you."
+exits = { "small_room" }
+
+fixtures = {
+	{
+		name = "bucket",
+		description = "A boring bucket. That alliteration was more interesting than this bucket is."
+	}
+}
