7db971b5e56f1a816bbf457a267d7353dcbe9f5a
Alek Ratzloff <alekratz@gmail.com>
2015-03-29 15:38:51 -0400

    Add a few basic vocab words

--- sharpadventure/Res/vocab.lua
+++ sharpadventure/Res/vocab.lua
@@ -1,1 +1,8 @@
-﻿
+﻿-- vocab.lua
+-- common vocabulary things. Mostly for synonyms.
+
+synonyms = {
+	CLOSE = { "SHUT" },
+	TAKE = { "GET", "GRAB" },
+	BASH = { "BREAK", "FORCE", "SMASH", "DESTROY" }
+}
