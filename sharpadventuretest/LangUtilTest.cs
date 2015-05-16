3bf4bbfecc747cb9a08fd01c85840f1a7eec27c3
Alek Ratzloff <alekratz@gmail.com>
2015-05-16 16:52:22 -0400

    Add some more language unit tests

--- sharpadventuretest/LangUtilTest.cs
+++ sharpadventuretest/LangUtilTest.cs
@@ -15,13 +15,22 @@ namespace sharpadventuretest
 				new string[] { "foo" },
 				new string[] { "foo", "bar" },
 				new string[] { "foo", "bar", "baz" },
-				new string[] { "ham", "eggs", "milk", "cheese" }
+				new string[] { "ham", "eggs", "milk", "cheese" },
+				new string[] { "shave and a haircut", "two bits" },
 			};
 
 			Assert.AreEqual (LangUtil.MakeEnglishList(lists [0]), "foo");
 			Assert.AreEqual (LangUtil.MakeEnglishList(lists [1]), "foo and bar");
 			Assert.AreEqual (LangUtil.MakeEnglishList(lists [2]), "foo, bar, and baz");
 			Assert.AreEqual (LangUtil.MakeEnglishList(lists [3]), "ham, eggs, milk, and cheese");
+			Assert.AreEqual (LangUtil.MakeEnglishList (lists [4]), "shave and a haircut and two bits");
+		}
+
+		[Test ()]
+		public void TestSplitPrepositions()
+		{
+			// can't do this one until we add a better way of loading the vocabulary
+			//Assert.AreEqual()
 		}
 	}
 }
